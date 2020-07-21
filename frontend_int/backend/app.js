4377be019be6f7fa1739e5858520912558db1a84
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-07-22 03:02:32 +0800

    frontend basic cookies

--- frontend_int/backend/app.js
+++ frontend_int/backend/app.js
@@ -5,9 +5,24 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+// Including parsing
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 // CORS
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+    'origin': 'http://localhost:3001',
+    credentials: true
+}));
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Headers","*");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
+});
 
 // Loading in controllers
 const controllers = require('./controllers');
--- frontend_int/backend/controllers.js
+++ frontend_int/backend/controllers.js
@@ -10,6 +10,25 @@ const controllers = {
                 'hello': 'world'
             });
         });
+        app.get('/basic/error', function(req, res){
+            res.status(400).send();
+        });
+        app.post('/basic/post', function(req, res){
+            console.log(req.body);
+            res.status(200).send(req.body);
+        });
+
+        // Cookies Fun
+        app.get('/cookies', function(req, res){
+            res
+            .cookie('userId', '123')
+            .cookie('authToken', '123abc')
+            .send();
+        });
+        app.get('/cookies/get', function(req, res){
+            console.log(req.cookies);
+            res.send(req.cookies);
+        });
     }
 };
 
